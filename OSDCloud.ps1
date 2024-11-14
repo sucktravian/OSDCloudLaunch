c93cb4a2181001775d5e4bc87d3a562218059441
Sucktravian <50853514+sucktravian@users.noreply.github.com>
2024-11-14 13:20:24 +0900

    Update OSDCloud.ps1

--- OSDCloud.ps1
+++ OSDCloud.ps1
@@ -1,12 +1,6 @@
 Write-Host  -ForegroundColor Cyan "Starting Custom OSDCloud ..."
 Start-Sleep -Seconds 5
 
-#Change Display Resolution for Virtual Machine
-if ((Get-MyComputerModel) -match 'Virtual') {
-    Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1600x"
-    Set-DisRes 1600
-}
-
 #Make sure I have the latest OSD Content
 Write-Host  -ForegroundColor Cyan "Updating OSD PowerShell Module"
 Install-Module OSD -Force
@@ -25,5 +19,5 @@ Write-Warning "I'm not sure of what to put here yet"
 #Restart from WinPE
 Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
 Start-Sleep -Seconds 20
-
+pause
 wpeutil reboot
