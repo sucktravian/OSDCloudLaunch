eb51eb67d985d63cd24719762c1b3f8cfbbf0d46
Sucktravian <50853514+sucktravian@users.noreply.github.com>
2024-11-14 11:30:29 +0900

    Add files via upload

---
+++ OSDCloud.ps1
@@ -0,0 +1,29 @@
+Write-Host  -ForegroundColor Cyan "Starting Custom OSDCloud ..."
+Start-Sleep -Seconds 5
+
+#Change Display Resolution for Virtual Machine
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1600x"
+    Set-DisRes 1600
+}
+
+#Make sure I have the latest OSD Content
+Write-Host  -ForegroundColor Cyan "Updating OSD PowerShell Module"
+Install-Module OSD -Force
+
+Write-Host  -ForegroundColor Cyan "Importing OSD PowerShell Module"
+Import-Module OSD -Force
+
+
+#Start OSDCloud ZTI change os when neeeded
+Write-Host  -ForegroundColor Cyan "Start OSDCloud with MY Parameters"
+Start-OSDCloud -OSName 'Windows 11 23H2 x64' -OSEdition Pro -OSLanguage ja-jp -OSActivation Retail -SkipAutopilot -ZTI
+
+Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
+Write-Warning "I'm not sure of what to put here yet"
+
+#Restart from WinPE
+Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+
+wpeutil reboot
