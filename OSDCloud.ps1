a688b64148ef6f92a2590cc88250c2b7be2f53e0
Sucktravian <50853514+sucktravian@users.noreply.github.com>
2024-11-15 10:46:14 +0900

    Update OSDCloud.ps1
    Testing unattend.xml in OSDCloud

--- OSDCloud.ps1
+++ OSDCloud.ps1
@@ -18,8 +18,23 @@ Write-Output $Global:MyOSDCloud
 Write-Host  -ForegroundColor Cyan "Start OSDCloud custom parameters"
 Start-OSDCloud -OSName 'Windows 11 23H2 x64' -OSEdition Pro -OSLanguage ja-jp -OSActivation Retail -SkipAutopilot -ZTI
 
-Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
-Write-Warning "I'm not sure of what to put here yet"
+Write-Host  -ForegroundColor Cyan "Downloading unattend.xml from repository"
+
+# Raw url
+$unattendURL = "https://raw.githubusercontent.com/sucktravian/OSDCloudLaunch/main/unattend.xml"
+
+# Path to the Sysprep folder
+$unattendPath = "X:\Windows\System32\Sysprep\unattend.xml"
+
+# Download the unattend.xml file from repository
+Invoke-WebRequest -Uri $unattendURL -OutFile $unattendPath
+
+# Verify if the download was successful
+if (Test-Path $unattendPath) {
+    Write-Host  -ForegroundColor Green "Unattend.xml downloaded successfully to $unattendPath"
+} else {
+    Write-Host  -ForegroundColor Red "Failed to download unattend.xml"
+}
 
 #Restart from WinPE
 Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
