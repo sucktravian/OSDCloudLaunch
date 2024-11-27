bc080ec6bbf136c993ce93d31db27afac50866f6
Sucktravian <50853514+sucktravian@users.noreply.github.com>
2024-11-27 15:41:24 +0900

    Create RunSysprep.ps1

---
+++ RunSysprep.ps1
@@ -0,0 +1,16 @@
+$unattendPath = "C:\OSD\temp\unattend.xml"
+Write-Warning "Searching for unattend.xml"
+    if (-not(Test-Path -Path $unattendPath )) {
+        Write-Host "unattend.xml not found" -ForegroundColor Red -ErrorAction Stop  
+    }
+    try {
+        $sysprep = "C:\Windows\System32\Sysprep\sysprep.exe"
+        $ar = "/generalize /reboot /oobe /unattend:$unattendPath"
+        Start-Process -FilePath $sysprep -ArgumentList $ar -Wait
+        Start-Sleep 2
+    }
+    catch {
+        Write-Host "sysprep failed to run"
+        $Error[0]
+        Pause
+    }
