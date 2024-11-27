2ce859ee8c37ef4a25ce611d7061721c7e6277a9
Sucktravian <50853514+sucktravian@users.noreply.github.com>
2024-11-27 15:33:32 +0900

    Update Testing.ps1

--- Testing.ps1
+++ Testing.ps1
@@ -6,11 +6,20 @@ if ((Get-MyComputerModel) -match 'Virtual') {
     Set-DisRes 1600
 }
 
-Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
-Install-Module OSD -Force
+Write-Host  -ForegroundColor Cyan "Starting Custom OSDCloud ..."
+Start-Sleep -Seconds 5
 
-Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
-Import-Module OSD -Force   
+Write-Host  -ForegroundColor Cyan "Updating OSD PowerShell Module"
+Install-Module OSD -Force -SkipPublisherCheck
+
+Write-Host  -ForegroundColor Cyan "Importing OSD PowerShell Module"
+Import-Module OSD -Force
+
+$Global:MyOSDCloud = [ordered]@{
+    OSImageIndex = '6'
+}
+
+Write-Output $Global:MyOSDCloud
 
 #=======================================================================
 #   [OS] Params and Start-OSDCloud
