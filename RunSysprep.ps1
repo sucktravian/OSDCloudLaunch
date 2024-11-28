3d201da9617ab8f202ed3b7cf83a2f4d0f38ee1f
Sucktravian <50853514+sucktravian@users.noreply.github.com>
2024-11-28 11:39:31 +0900

    test all

--- RunSysprep.ps1
+++ RunSysprep.ps1
@@ -1,3 +1,21 @@
+Import-Module OSD -Force
+Invoke-Expression (Invoke-RestMethod sandbox.osdcloud.com)
+Invoke-Expression (Invoke-RestMethod functions.osdcloud.com)
+# Check if bitlocker is active and disable it
+$bitlockerStatus = Get-BitLockerVolume -MountPoint "C:"
+
+if ($bitlockerStatus.ProtectionStatus -eq "On") {
+    Write-Host "BitLocker is enabled on drive C:. Disabling BitLocker..." -ForegroundColor Yellow
+    Disable-BitLocker -MountPoint "C:"
+    Write-Host "Decryption process started. Monitor progress with Get-BitLockerVolume." -ForegroundColor Green
+} else {
+    Write-Host "BitLocker is already disabled on drive C:." -ForegroundColor Green
+}
+# Update Drivers
+UpdateDrivers
+UpdateWindows
+
+Pause
 $unattendPath = "C:\OSDCloud\Temp\unattend.xml"
 Write-Warning "Searching for unattend.xml"
     if (-not(Test-Path -Path $unattendPath )) {
--- Testing.ps1
+++ Testing.ps1
@@ -38,56 +38,7 @@ Start-OSDCloud @Params
 #================================================
 #  [PostOS] OOBEDeploy Configuration
 #================================================
-Write-Host -ForegroundColor Green "Create C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json"
-$OOBEDeployJson = @'
-{
-    "AddNetFX3":  {
-                      "IsPresent":  true
-                  },
-    "Autopilot":  {
-                      "IsPresent":  false
-                  },
-    "RemoveAppx":  [
-                    "MicrosoftTeams",
-                    "Microsoft.BingWeather",
-                    "Microsoft.BingNews",
-                    "Microsoft.GamingApp",
-                    "Microsoft.GetHelp",
-                    "Microsoft.Getstarted",
-                    "Microsoft.Messaging",
-                    "Microsoft.MicrosoftOfficeHub",
-                    "Microsoft.MicrosoftSolitaireCollection",
-                    "Microsoft.MicrosoftStickyNotes",
-                    "Microsoft.MSPaint",
-                    "Microsoft.People",
-                    "Microsoft.PowerAutomateDesktop",
-                    "Microsoft.StorePurchaseApp",
-                    "Microsoft.Todos",
-                    "microsoft.windowscommunicationsapps",
-                    "Microsoft.WindowsFeedbackHub",
-                    "Microsoft.WindowsMaps",
-                    "Microsoft.WindowsSoundRecorder",
-                    "Microsoft.Xbox.TCUI",
-                    "Microsoft.XboxGameOverlay",
-                    "Microsoft.XboxGamingOverlay",
-                    "Microsoft.XboxIdentityProvider",
-                    "Microsoft.XboxSpeechToTextOverlay",
-                    "Microsoft.YourPhone",
-                    "Microsoft.ZuneMusic",
-                    "Microsoft.ZuneVideo"
-                   ],
-    "UpdateDrivers":  {
-                          "IsPresent":  true
-                      },
-    "UpdateWindows":  {
-                          "IsPresent":  true
-                      }
-}
-'@
-If (!(Test-Path "C:\ProgramData\OSDeploy")) {
-    New-Item "C:\ProgramData\OSDeploy" -ItemType Directory -Force | Out-Null
-}
-$OOBEDeployJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json" -Encoding ascii -Force
+
 
 #================================================
 #  [PostOS] OOBE CMD Command Line
@@ -116,9 +67,6 @@ if (Test-Path $unattendPath) {
     Write-Host  -ForegroundColor Red "Unattend.xml was not found in the Sysprep folder."
 }
 
-Invoke-RestMethod "https://raw.githubusercontent.com/sucktravian/OSDCloudLaunch/main/RunSysprep.ps1" | Out-File -FilePath 'C:\Windows\Setup\scripts\RunSysprep.ps1' -Encoding ascii -Force
-
-
 
 $OOBECMD = @'
 @echo off
@@ -128,7 +76,7 @@ start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scri
 
 exit 
 '@
-#$OOBECMD | Out-File -FilePath 'C:\Windows\Setup\scripts\oobe.cmd' -Encoding ascii -Force
+$OOBECMD | Out-File -FilePath 'C:\Windows\Setup\scripts\oobe.cmd' -Encoding ascii -Force
 
 #=======================================================================
 #   Restart-Computer
