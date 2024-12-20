52ae23e849873d65d5e3452094a45ec8c5537096
Sucktravian <50853514+sucktravian@users.noreply.github.com>
2024-12-20 13:14:27 +0900

    Create Run-OSDCloudGUI.ps1

---
+++ Run-OSDCloudGUI.ps1
@@ -0,0 +1,15 @@
+#================================================
+#   [PreOS] Update Module
+#================================================
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host  -ForegroundColor Green "Setting Display Resolution to 1600x"
+    Set-DisRes 1600
+}
+
+Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
+Install-Module OSD -Force
+
+Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
+Import-Module OSD -Force 
+
+Start-OSDCloudGUI -BrandName "Custom OSD Base Image by F."
