371f7da106232bd233f58d55116a518a84d5d863
Sucktravian <50853514+sucktravian@users.noreply.github.com>
2024-12-20 15:24:36 +0900

    Update Testing.ps1

--- Testing.ps1
+++ Testing.ps1
@@ -1,48 +1,4 @@
-#================================================
-#   [PreOS] Update Module
-#================================================
-if ((Get-MyComputerModel) -match 'Virtual') {
-    Write-Host  -ForegroundColor Green "Setting Display Resolution to 1600x"
-    Set-DisRes 1600
-}
 
-Write-Host  -ForegroundColor Cyan "Starting Custom OSDCloud ..."
-Start-Sleep -Seconds 5
-
-Write-Host  -ForegroundColor Cyan "Updating OSD PowerShell Module"
-Install-Module OSD -Force -SkipPublisherCheck
-
-Write-Host  -ForegroundColor Cyan "Importing OSD PowerShell Module"
-Import-Module OSD -Force
-
-$Global:MyOSDCloud = [ordered]@{
-    OSImageIndex = '6'
-}
-
-Write-Output $Global:MyOSDCloud
-
-#=======================================================================
-#   [OS] Params and Start-OSDCloud
-#=======================================================================
-$Params = @{
-    OSVersion = "Windows 11"
-    OSBuild = "23H2"
-    OSEdition = "Pro"
-    OSLanguage = "ja-jp"
-    OSLicense = "Retail"
-    ZTI = $true
-    Firmware = $false
-}
-Start-OSDCloud @Params
-
-#================================================
-#  [PostOS] OOBEDeploy Configuration
-#================================================
-
-
-#================================================
-#  [PostOS] OOBE CMD Command Line
-#================================================
 Write-Host -ForegroundColor Green "Downloading and creating script for OOBE phase"
 
 Write-Host  -ForegroundColor Cyan "Downloading unattend.xml from repository"
@@ -67,18 +23,3 @@ if (Test-Path $unattendPath) {
     Write-Host  -ForegroundColor Red "Unattend.xml was not found in the Sysprep folder."
 }
 
-
-$OOBECMD = @'
-@echo off
-# Execute OOBE Tasks
-start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\RunSysprep.ps1
-exit 
-'@
-$OOBECMD | Out-File -FilePath 'C:\Windows\Setup\scripts\oobe.cmd' -Encoding ascii -Force
-
-#=======================================================================
-#   Restart-Computer
-#=======================================================================
-Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
-Start-Sleep -Seconds 20
-wpeutil reboot
