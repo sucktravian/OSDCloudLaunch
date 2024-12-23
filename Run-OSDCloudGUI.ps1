4e107590ce5af41401b7f5479da592ba9b6cd9ae
Sucktravian <50853514+sucktravian@users.noreply.github.com>
2024-12-23 11:43:19 +0900

    Update Run-OSDCloudGUI.ps1

--- Run-OSDCloudGUI.ps1
+++ Run-OSDCloudGUI.ps1
@@ -13,3 +13,72 @@ Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
 Import-Module OSD -Force 
 
 Start-OSDCloudGUIDEV
+
+$OOBEDeployJson = @'
+{
+    "AddNetFX3":  {
+                      "IsPresent":  true
+                  },
+    "Autopilot":  {
+                      "IsPresent":  false
+                  },
+    "RemoveAppx":  [
+                    "Microsoft.Microsoft3DViewer",
+                    "Microsoft.BingSearch",
+                    "Clipchamp.Clipchamp",
+                    "Microsoft.Windows.DevHome",
+                    "MicrosoftCorporationII.MicrosoftFamily",
+                    "Microsoft.MixedReality.Portal",
+                    "Microsoft.Office.OneNote",
+                    "Microsoft.OutlookForWindows",
+                    "MicrosoftTeams",
+                    "MSTeams",
+                    
+                    "MicrosoftCorporationII.QuickAssist",
+                    "Microsoft.SkypeApp",
+                    "Microsoft.BingWeather",
+                    "Microsoft.BingNews",
+                    "Microsoft.GamingApp",
+                    "Microsoft.GetHelp",
+                    "Microsoft.Getstarted",
+                    "Microsoft.Messaging",
+                    "Microsoft.MicrosoftOfficeHub",
+                    "Microsoft.MicrosoftSolitaireCollection",
+                    "Microsoft.People",
+                    "Microsoft.PowerAutomateDesktop",
+                    "Microsoft.StorePurchaseApp",
+                    "Microsoft.Todos",
+                    "Microsoft.Wallet",
+                    "Microsoft.XboxApp",
+                    "microsoft.windowscommunicationsapps",
+                    "Microsoft.WindowsFeedbackHub",
+                    "Microsoft.WindowsMaps",
+                    "Microsoft.WindowsSoundRecorder",
+                    "Microsoft.Xbox.TCUI",
+                    "Microsoft.XboxGameOverlay",
+                    "Microsoft.XboxGamingOverlay",
+                    "Microsoft.XboxIdentityProvider",
+                    "Microsoft.XboxSpeechToTextOverlay",
+                    "Microsoft.YourPhone",
+                    "Microsoft.ZuneMusic",
+                    "Microsoft.ZuneVideo"
+                   ],
+    "UpdateDrivers":  {
+                          "IsPresent":  true
+                      },
+    "UpdateWindows":  {
+                          "IsPresent":  true
+                      }
+}
+'@
+If (!(Test-Path "C:\ProgramData\OSDeploy")) {
+    New-Item "C:\ProgramData\OSDeploy" -ItemType Directory -Force | Out-Null
+}
+$OOBEDeployJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json" -Encoding ascii -Force
+
+#=======================================================================
+#   Restart-Computer
+#=======================================================================
+Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot
