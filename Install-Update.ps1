0cc6dc9a58a8edc9a2e86f1e1fe687f9e37cc465
Sucktravian <50853514+sucktravian@users.noreply.github.com>
2024-12-24 09:02:57 +0900

    Update Install-Update.ps1

--- Install-Update.ps1
+++ Install-Update.ps1
@@ -7,16 +7,6 @@ if (!(Get-Module PSWindowsUpdate -ListAvailable)) {
         Write-Warning 'Unable to install PSWindowsUpdate Windows Updates'
     }
 }
-<#
-if (Get-Module PSWindowsUpdate -ListAvailable -ErrorAction Ignore) {
-    Write-Host -ForegroundColor DarkCyan 'Add-WUServiceManager -MicrosoftUpdate -Confirm:$false'
-    Add-WUServiceManager -MicrosoftUpdate -Confirm:$false
-    Write-Host -ForegroundColor DarkCyan 'Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot'
-    Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot -NotTitle 'Preview' -NotKBArticleID 'KB890830', 'KB5005463', 'KB4481252'
-}
-#>
 Import-Module OSD -Force
 Invoke-Expression (Invoke-RestMethod functions.osdcloud.com)
-# Update Drivers
-UpdateDrivers
 UpdateWindows
