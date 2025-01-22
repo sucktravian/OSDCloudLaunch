46cdce59c56c729bd3700587dadd365d9918353a
Sucktravian <50853514+sucktravian@users.noreply.github.com>
2025-01-22 15:32:40 +0900

    Update Run-OSDCloudGUI.ps1

--- Run-OSDCloudGUI.ps1
+++ Run-OSDCloudGUI.ps1
@@ -7,7 +7,8 @@ if ((Get-MyComputerModel) -match 'Virtual') {
 }
 
 Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
-Install-Module OSD -Force -SkipPublisherCheck
+#Install-Module OSD -Force -SkipPublisherCheck
+Update-Module OSD -Force  
 
 Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
 Import-Module OSD -Force 
