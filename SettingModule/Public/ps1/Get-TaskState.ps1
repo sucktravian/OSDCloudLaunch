31b0f03062be381bc3bfcab85929a8e71547f9bb
Sucktravian <50853514+sucktravian@users.noreply.github.com>
2025-10-14 09:31:28 +0900

    Delete SettingModule directory

--- SettingModule/Public/ps1/Get-TaskState.ps1
+++
@@ -1,6 +0,0 @@
-function Get-TaskState {
-    if (Test-Path $global:TaskStateFile) {
-        return (Get-Content -Path $global:TaskStateFile | ConvertFrom-Json).LastCompletedTask
-    }
-    return $null
-}
--- SettingModule/Public/ps1/Save-TaskState.ps1
+++
@@ -1,5 +0,0 @@
-function Save-TaskState {
-    param ([string]$TaskName)
-    $state = @{ LastCompletedTask = $TaskName }
-    $state | ConvertTo-Json | Set-Content -Path $global:TaskStateFile
-}
