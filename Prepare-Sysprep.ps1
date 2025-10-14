2bb3a5e9940813ca710be9a91b7cae541a7abec1
Sucktravian <50853514+sucktravian@users.noreply.github.com>
2025-10-14 09:30:50 +0900

    Delete Prepare-Sysprep.ps1

--- Prepare-Sysprep.ps1
+++
@@ -1,9 +0,0 @@
-# Import the Setting Module
-Import-Module "C:\OSDCloud\Scripts\SettingModule\Setting.psm1"
-
-# Remove Task
-Set-RbRsScript -settask $false
-
-# Add Task for running script after sysprep
-Set-RbRsScript -settask $true -ScriptName "C:\OSDCloud\Scripts\Checker.ps1"
-
